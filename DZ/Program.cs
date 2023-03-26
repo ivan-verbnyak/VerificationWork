1a597d060fff4be0f852219b264fabcaad6c7bdb
ivan-verbnyak <kadet717@ya.ru>
2023-03-26 22:49:34 +0500

    cod gotov

--- DZ/Program.cs
+++ DZ/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿Console.WriteLine("Input array: ");
+string[] line = new string[5];
+var resalt = 0;
+
+for (int i = 0; i < line.Length; i++)
+{
+    line[i] = Console.ReadLine();
+}
+foreach (var item in line)
+{
+    if (item.Length <= 3)
+    {
+        Console.Write($"{item}, ");
+    }
+}
+Console.WriteLine();
